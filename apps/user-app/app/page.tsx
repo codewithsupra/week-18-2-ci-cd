98a6d30179002ff6d1b52edc33e06902c94ace41
Supratim Sarkar <supratimsarkar@Supratims-MacBook-Air.local>
2025-05-13 10:44:06 +0400

    done changes

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -4,7 +4,10 @@ import { authOptions } from "./lib/auth";
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
+  //we are getting the user credentials of the person that is logged in 
+
   if (session?.user) {
+    console.log("user logged in",session?.user)
     redirect('/dashboard')
   } else {
     redirect('/api/auth/signin')
